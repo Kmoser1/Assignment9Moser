73935071ab27748f65091ea32015a9de698c7bd8
Kmoser1 <kmoser1@walsh.edu>
2025-10-31 21:32:59 -0400

    works fine now, I had made some changes and I thought it wasn't working, but I had previously closed and reopened Visual Studio Code and forgot to re-enable liveserver. Added user input to search query / endpoint.

--- js/script.js
+++ js/script.js
@@ -7,19 +7,26 @@ const fetchButton = document.querySelector('#fetch-gif-btn');
 // Initialize images array
 let images = [];
 
+//User-input test
+const searchInput = document.querySelector('#search-input');
+
 // Function to fetch GIFs from GIPHY API
 async function fetchGIFs() {
-  const searchTerm = 'apple'; // or any search term
-  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=DdoeyWowjJgDqhustN7kKkmOxNIWVTlk&q=${searchTerm}&limit=10&offset=0&rating=g&lang=en`;
+  const searchTerm = searchInput.value.trim();
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=DdoeyWowjJgDqhustN7kKkmOxNIWVTlk&q=${searchTerm}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
 
   // good practice to try and catch if something goes wrong(?)
   try {
-    const response = await fetch(url);
+    const response = await fetch(endpoint);
     const data = await response.json();
 
     // Extract original image URLs and store in images array
     images = data.data.map(gif => gif.images.original.url);
+
+    // debugging step, shows source GIF links in logs
+    console.log(images)
   }
+  
   catch (error) {
     console.error('Error fetching GIFs:', error);
   }
